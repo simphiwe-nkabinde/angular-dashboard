cd01b1da20d4cbc6895d19cce505e51b2103f513
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-06-02 13:46:01 +0200

    create forgot-password page feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'forgot-password', component:ForgotPasswordComponent}
 ];
 
 @NgModule({
