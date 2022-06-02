4094bc931a5861440fda14811ef82b20e3cf0a3e
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-06-02 12:25:13 +0200

    generate main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    PageContentComponent
   ],
   imports: [
     BrowserModule,
